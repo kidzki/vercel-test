ecf510d991c3e14a2084e76ff9614312caddeae0
Michael Kick <hire@kidzki.dev>
2023-02-17 03:06:27 +0100

    MASHALLAH

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,24 @@ import { Session } from 'next-auth';
 import { headers } from 'next/headers';
 import AuthContext from './AuthContext';
 
-// async function getSession(cookie: string): Promise<Session> {
-  // const response = await fetch(`${process.env.LOCAL_AUTH_URL}/api/auth/session`, {
-  //   headers: {
-  //     cookie,
-  //   },
-  // });
+async function getSession(cookie: string): Promise<Session> {
+  const response = await fetch(`${process.env.LOCAL_AUTH_URL}/api/auth/session`, {
+    headers: {
+      cookie,
+    },
+  });
 
-  // const session = await response.json();
+  const session = await response.json();
 
-  // return Object.keys(session).length > 0 ? session : null;
-// }
+  return Object.keys(session).length > 0 ? session : null;
+}
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  // const session = await getSession(headers().get('cookie') ?? '');
-  const session = true;
+  const session = await getSession(headers().get('cookie') ?? '');
   return (
     <html lang="en">
       {/*
@@ -30,9 +29,9 @@ export default async function RootLayout({
       */}
       <head />
       <body>
-      {/* <AuthContext session={session}> */}
+      <AuthContext session={session}>
           {children}
-      {/* </AuthContext> */}
+      </AuthContext>
       </body>
     </html>
   )
